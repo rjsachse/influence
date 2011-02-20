f75412c06e3ade301804de7f68cf4c7e6c8e23dc
thierry.rioual <thierry.rioual@5d6360d2-3c46-c574-2d01-d3da462cefa9>
2011-02-20 10:49:22 +0000

    v1.2.1 Add library editor Add Night engine Add Plugin Weather.com plus Fix some issues

--- trunk/Skin/scripts/script.influence_view.installer/default.py
+++ trunk/Skin/scripts/script.influence_view.installer/default.py
@@ -21,38 +21,16 @@ from xbmcaddon import Addon
 SILENT = True
 DIALOG_PROGRESS = xbmcgui.DialogProgress()
 
-
-def download( url, destination="special://home/addons/packages/" ):
-    try:
-        if not SILENT:
-            DIALOG_PROGRESS.create( __settings__.getAddonInfo( "name" ) )
-        destination = xbmc.translatePath( destination ) + os.path.basename( url )
-        def _report_hook( count, blocksize, totalsize ):
-            if not SILENT: 
-                percent = int( float( count * blocksize * 100 ) / totalsize )
-                DIALOG_PROGRESS.update( percent, "Downloading: %s " % url, "to: %s" % destination )
-        fp, h = urllib.urlretrieve( url, destination, _report_hook )
-        print fp, h
-        return fp
-    except:
-        print_exc()
-    if not SILENT:
-        DIALOG_PROGRESS.close()
-    return ""
-
-
 def install( filename ):
     from resources.lib.extractor import extract
     print "extract : %s %s " % (filename, xbmc.translatePath( "special://skin" ) )
     return extract( filename, xbmc.translatePath( "special://skin" ) )
-
 
 def notification( header="", message="", sleep=5000, icon="" ):
     """ Will display a notification dialog with the specified header and message,
         in addition you can set the length of time it displays in milliseconds and a icon image. 
     """
     xbmc.executebuiltin( "XBMC.Notification(%s,%s,%i,%s)" % ( header, message, sleep, icon ) )
-
 
 if ( __name__ == "__main__" ):
     try: zipview = sys.argv[ 1 ]
@@ -62,11 +40,11 @@ if ( __name__ == "__main__" ):
         except: print_exc()
 
         if zipview:
-            ListView = ['1001','1002','1003','1004','1005','1006','1007','1008','1009','1010']
+            ListView = ['1001','1002','1003','1004','1005','1006','1007','1008','1009','1010','1011','1012','1013','1014','1015','1016','1017','1018','1019','1020']
             ViewOK=False
             for v in ListView:
                 print v;
-                if xbmc.getCondVisibility("!Skin.HasSetting(ViewCustom%s_IsInstall)" % (v) ):
+                if xbmc.getCondVisibility("!Skin.HasSetting(ViewCustom%s_IsInstall)" % (v) ) or os.access(xbmc.translatePath('special://skin/720p/View_Custom%s.xml' % v),os.F_OK)==False:
                     print zipview
                     fp, ok = install( zipview )
                     SrcView=open('special://skin/720p/View_Custom.xml','r')
@@ -78,6 +56,8 @@ if ( __name__ == "__main__" ):
                     NewView.close
                     ViewOK=True
                     xbmc.executebuiltin( "Skin.SetBool(ViewCustom%s_IsInstall)" % ( v ) )
+                    try: os.remove(xbmc.translatePath('special://skin/720p/View_Custom.xml'))
+                    except: print "erreur os.remove(%s)" % xbmc.translatePath('special://skin/720p/View_Custom.xml')
                     break	        
 
             if ViewOK:
